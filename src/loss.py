18365940e406d10fda1c0d70583dd674ae7cb610
salwaElKaddaoui <elkaddaoui.salwa@gmail.com>
2025-02-09 22:12:03 +0100

    add loss function

---
+++ src/loss.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+def loss(self, logits, labels):
+    # pixel-wise softmax
+    softmax = tf.nn.softmax(logits, axis=-1)
+    softmax = tf.clip_by_value(softmax, 1e-7, 1.0 - 1e-7) #to avoid log(0)
+    
+    # cross-entropy loss
+    loss = -tf.reduce_sum(labels * tf.math.log(softmax), axis=-1)
+    return tf.reduce_mean(loss)
