0ed113c38e7af3ff7966dd6acc61f88c9c3d8bb6
salwaElKaddaoui <elkaddaoui.salwa@gmail.com>
2025-02-12 21:20:15 +0100

    add shift to data augmentation

--- src/data.py
+++ src/data.py
@@ -18,16 +18,41 @@ class DataProcessor:
 
     def preprocess(self, image, mask):
         image = tf.image.resize(image, self.img_size, method='bilinear') / 255.0  # Normalize between 0 and 1
-        mask = tf.image.resize(mask, self.img_size, method='nearest')  # Normalize between 0 and 1
+        mask = tf.image.resize(mask, self.img_size, method='nearest')
         mask = tf.one_hot(mask, depth=self.num_classes)        # One-hot encode (the depth dim. is one hot encoded)
         mask = tf.squeeze(mask, axis=-2)
         return image, mask
 
     def augment(self, image, mask):
-        if tf.random.uniform(()) > 0.5:
-            image = tf.image.rot90(image)
-            mask = tf.image.rot90(mask)
+        choice = tf.random.uniform((), minval=0, maxval=2, dtype=tf.int32)
+        
+        def rotate():
+            return tf.image.rot90(image), tf.image.rot90(mask)
+        
+        def shift():
+            """
+            shift by padding and cropping
+            """
+            pad_left = tf.random.uniform((), minval=0, maxval=50, dtype=tf.int32)
+            pad_top = tf.random.uniform((), minval=0, maxval=50, dtype=tf.int32)
+            
+            if tf.random.uniform(()) > 0.5: #shift to the right 
+                # 1. pad
+                padded_image = tf.pad(image, [[0, 0], [pad_left, 0], [0, 0]], constant_values=0)
+                padded_mask = tf.pad(mask, [[0, 0], [pad_left, 0], [0, 0]], constant_values=0)
+                # 2. crop
+                return padded_image[:image.shape[0], :image.shape[1], :], padded_mask[:mask.shape[0], :mask.shape[1], :]
+                
+            else: #shift to the bottom
+                # 1. pad
+                padded_image = tf.pad(image, [[pad_top, 0], [0, 0], [0, 0]], constant_values=0)
+                padded_mask = tf.pad(mask, [[pad_top, 0], [0, 0], [0, 0]], constant_values=0)
+                # 2. crop
+                return padded_image[:image.shape[0], :image.shape[1], :], padded_mask[:mask.shape[0], :mask.shape[1], :]
+
+        image, mask = tf.switch_case(choice, branch_fns={0: rotate, 1: shift})
         return image, mask
+
 
     def create_dataset(self, image_paths, mask_paths, training=True):
         dataset = tf.data.Dataset.from_tensor_slices((image_paths, mask_paths))
@@ -35,4 +60,6 @@ class DataProcessor:
         if training:
             dataset = dataset.map(self.augment, num_parallel_calls=tf.data.AUTOTUNE)
         dataset = dataset.map(self.preprocess, num_parallel_calls=tf.data.AUTOTUNE)
-        return dataset.shuffle(10).batch(self.batch_size).prefetch(tf.data.AUTOTUNE)
+        return dataset.shuffle(100).batch(self.batch_size).prefetch(tf.data.AUTOTUNE)
+
+
