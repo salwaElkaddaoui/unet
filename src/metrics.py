a3958bed0d25899d84bec8cfe20dff152ae84bd4
salwaElKaddaoui <elkaddaoui.salwa@gmail.com>
2025-02-15 23:19:10 +0100

    fix iou

--- src/metrics.py
+++ src/metrics.py
@@ -1,27 +1,17 @@
 import tensorflow as tf
 
-def compute_iou(y_pred, y_true, num_classes):
-    """
-    num_classes = number of foreground classes + 1 (we add 1 for the background)
-    """
-    y_pred = tf.argmax(y_pred, axis=-1)
-    iou_per_class = []
-    
-    for class_id in range(num_classes):
-        true_class = tf.equal(y_true, class_id)  
-        pred_class = tf.equal(y_pred, class_id)  
-        
-        intersection = tf.reduce_sum(tf.cast(tf.logical_and(true_class, pred_class), tf.float32))
-        union = tf.reduce_sum(tf.cast(tf.logical_or(true_class, pred_class), tf.float32))
-        
-        iou = tf.math.divide_no_nan(intersection, union)
-        iou_per_class.append(iou)
-    
-    return tf.reduce_mean(iou_per_class)
+def compute_iou(y_true, y_pred):
+    num_classes = tf.shape(y_true)[-1]
+    y_pred = tf.one_hot(tf.argmax(y_pred, axis=-1), depth=num_classes, dtype=tf.float32)
+    intersection = tf.reduce_sum(y_true * y_pred, axis=[0, 1, 2]) # sum over [batch, H, W], returns [Ch]
+    union = tf.reduce_sum(y_true + y_pred, axis=[0, 1, 2]) - intersection
+    iou = intersection / tf.maximum(union, 1e-10)
+    miou = tf.reduce_mean(iou)
+    return iou, miou
 
 def compute_pixel_accuracy(y_pred, y_true):
-    y_pred = tf.argmax(y_pred, axis=-1)  # get the class index
-    correct = tf.equal(y_pred, y_true)  
+    y_pred = tf.argmax(y_pred, axis=-1)  # get the estimated class index
+    correct = tf.equal(y_pred, tf.argmax(y_true, axis=-1))  
     accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
     return accuracy
 
