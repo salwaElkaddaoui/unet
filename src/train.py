5a11d8ecfddaab4e898c084d28e0265b33944421
salwaElKaddaoui <elkaddaoui.salwa@gmail.com>
2025-02-15 23:38:53 +0100

    add metrics and tensorboard logs to training

--- src/train.py
+++ src/train.py
@@ -1,12 +1,14 @@
 import tensorflow as tf
-from loss import loss
-from model import Unet
-from data import DataProcessor
-import os, sys
-# from model_seq import UNet
+# tf.debugging.set_log_device_placement(True)
 
-import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+# import os
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+from metrics import compute_iou, compute_pixel_accuracy
+
+import datetime
+log_dir = "logs/train_" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+
 
 class Trainer:
     def __init__(self, loss_fn, optimizer):
@@ -14,55 +16,41 @@ class Trainer:
         self.optimizer = optimizer
 
     @tf.function
-    def train_step(self, model, image, mask):
-        with tf.GradientTape() as tape:
-            mask_pred = model(image)
-            loss = self.loss_fn(mask_pred, mask)
-        gradients = tape.gradient(loss, model.trainable_variables)
-        self.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-        return loss
+    def train_iteration(self, model, image_batch, mask_batch):
+        with tf.device('/GPU:0'):
+            with tf.GradientTape() as tape:
+                mask_pred = model(image_batch)
+                loss = self.loss_fn(mask_pred, mask_batch)
+                iou, miou = compute_iou(mask_batch, mask_pred)
+                pixel_accuracy = compute_pixel_accuracy(mask_batch, mask_pred)
+            gradients = tape.gradient(loss, model.trainable_variables)
+            self.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+        return loss, miou, pixel_accuracy
+
 
     def __call__(self, model, dataset, epochs):
+        writer = tf.summary.create_file_writer(log_dir)
         for epoch in range(epochs):
-            for step, (image_batch, mask_batch) in enumerate(dataset):
-                loss = self.train_step(model, image_batch, mask_batch)
-                if step % 10 == 0:
-                    print(f"Epoch {epoch+1}, Step {step}, Loss: {loss.numpy():.4f}")
+            total_loss = 0.0
+            total_iou = 0.0
+            total_pixel_accuracy = 0.0
+            num_iterations = 0.0
+            for iteration, (image_batch, mask_batch) in enumerate(dataset):
+                with tf.device('/GPU:0'):
+                    loss, miou, pixel_accuracy = self.train_iteration(model, image_batch, mask_batch)
+                print(f"Epoch {epoch}, Iteration {iteration}, Loss: {loss.numpy():.4f}, Pixel Accuracy: {pixel_accuracy}")
+            
+                total_loss += loss
+                total_iou += miou
+                total_pixel_accuracy += pixel_accuracy
+                num_iterations += 1.
+            
+            avg_loss = total_loss / num_iterations
+            avg_iou = total_iou / num_iterations
+            avg_pixel_accuracy = total_pixel_accuracy / num_iterations
 
-if __name__=='__main__':
-
-    optimizer = tf.optimizers.Adam(learning_rate=1e-3)
-    trainer = Trainer(loss_fn=loss, optimizer=optimizer)
-    model = Unet(in_image_depth=3, nb_classes=5, use_batchnorm=False)
-    print("Trainable Variables:", model.trainable_variables)
-
-    # sys.exit()
-    # model = UNet(num_classes=5)
-
-    img_size, batch_size, num_classes = [256, 256], 16, 5
-    dataprocessor = DataProcessor(img_size, batch_size, num_classes)
-
-    image_folder = "/home/salwa/Documents/code/smartcar/dataset3"
-    annotation_folder = "/home/salwa/Documents/code/smartcar/grayscale_masks_3"
-
-    if not os.path.exists(image_folder):
-        raise FileNotFoundError(f"Folder not found: {image_folder}")
-
-    if not os.path.exists(annotation_folder):
-        raise FileNotFoundError(f"Folder not found: {annotation_folder}")
-    
-    image_filenames = os.listdir(image_folder)
-    annotation_filenames = os.listdir(annotation_folder)
-    
-    print(len(image_filenames), len(annotation_filenames))
-
-    # keep only the images whose annotations exist
-    image_filenames = [ f for f in image_filenames if f[:-3]+"png" in annotation_filenames]
-    print(len(image_filenames))
-
-    image_paths = [os.path.join(image_folder, f) for f in image_filenames]
-    mask_paths = [os.path.join(annotation_folder, f[:-3]+"png") for f in image_filenames]
-
-    train_dataset = dataprocessor.create_dataset(image_paths, mask_paths, training=True)
-
-    trainer(model, train_dataset, epochs=10)
+            # write to tensorboard
+            with writer.as_default():
+                tf.summary.scalar(name="Loss", data=avg_loss, step=epoch)
+                tf.summary.scalar(name="Mean IoU", data=avg_iou, step=epoch)
+                tf.summary.scalar(name="Pixel Accuracy", data=avg_pixel_accuracy, step=epoch)
