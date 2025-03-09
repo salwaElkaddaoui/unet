cc8af38d5ebbf7e65649b96eeac2f6242f3bd7fb
salwaElKaddaoui <elkaddaoui.salwa@gmail.com>
2025-03-09 20:02:22 +0100

    rename loss arguments

--- src/loss.py
+++ src/loss.py
@@ -1,10 +1,6 @@
 import tensorflow as tf
 
-def loss(predictions, labels):
-    # pixel-wise softmax
-    # softmax = tf.nn.softmax(logits, axis=-1)
-    # softmax = tf.clip_by_value(softmax, 1e-7, 1.0 - 1e-7) #to avoid log(0)
-    
+def loss(y_pred, y_true):
     # cross-entropy loss
-    loss = -tf.reduce_sum(labels * tf.math.log(predictions), axis=-1)
+    loss = -tf.reduce_sum(y_true * tf.math.log(y_pred), axis=-1)
     return tf.reduce_mean(loss)
